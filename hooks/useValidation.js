a948db8d1feddfb3c6b26cb1efe83a1d18000201
Ryan Ye <awj_yjiyang@hotmail.com>
2021-03-06 22:15:48 -0800

    Removed the useValidation.js file

--- hooks/useValidation.js
+++
@@ -1,9 +0,0 @@
-export default function useValidation(form) {
-    form.map(fe => {
-        let {required, value}  = fe;
-        if(required && value === '' ){
-            fe.has_error = true;
-        }
-    });
-    return form;
-}
