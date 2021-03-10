2ee916a563dda9bba04dc1b2f768325fccc52b02
Ryan Ye <awj_yjiyang@hotmail.com>
2021-03-10 11:05:09 -0800

    Add meta tag to the header
    Add seo meta tags to the header

--- PageComps/HeaderComps/Header.js
+++ PageComps/HeaderComps/Header.js
@@ -2,12 +2,16 @@ import Head from 'next/head';
 export default function Header(props) {
 
     //  SEOs are here
-    const {title} = props;
+    const {title, description, keywords} = props;
     return (
         <Head>
             <title>{title}</title>
             <meta property="og:title" content={title} key="title" />
             <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            {(!!description) && <meta name="description" content={description}/>}
+            {(!!keywords) && <meta name="keywords" content={keywords}/>}
+            <meta name="author" content="Zaia"/>
         </Head>
     )
 }
